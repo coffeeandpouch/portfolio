4833350c4d85d88b08789df8456243f9ccc4fb95
SAM EINSIDLER <sameinsidler@SAMs-Air.attlocal.net>
2020-08-05 19:51:27 -0500

    latest changes

---
+++ index.js
@@ -0,0 +1,22 @@
+$("#aboutMe").click(function() {
+$('html, body').animate({
+   scrollTop: $("#aboutMe").offset().top},
+   'slow'); 
+});
+
+$("#contactMe").click(function() {
+   $('html, body').animate({
+      scrollTop: $("#contactMe").offset().top},
+      'slow');
+   })
+
+$("#myProjects").click(function() {
+   $('html, body').animate({
+      scrollTop: $("#myProjects").offset().top},
+      'slow');
+
+   })
+
+
+  
+
