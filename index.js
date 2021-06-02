5d9546dd0f9bdc542ee8abf11973181939dd9ad1
coffeeandpouch <sameinsidler@me.com>
2021-06-02 10:06:12 -0500

    changes to styling

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-$("#aboutMe").click(function() {
-$('html, body').animate({
-   scrollTop: $("#aboutMe").offset().top},
-   'slow'); 
+$("#aboutMe").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#aboutMe").offset().top,
+    },
+    "slow"
+  );
 });
 
-$("#contactMe").click(function() {
-   $('html, body').animate({
-      scrollTop: $("#contactMe").offset().top},
-      'slow');
-   })
+$("#contactMe").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#contactMe").offset().top,
+    },
+    "slow"
+  );
+});
 
-$("#myProjects").click(function() {
-   $('html, body').animate({
-      scrollTop: $("#myProjects").offset().top},
-      'slow');
-
-   })
-
-
-  
-
+$("#myProjects").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $("#myProjects").offset().top,
+    },
+    "slow"
+  );
+});
